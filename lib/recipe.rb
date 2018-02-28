a571d9090de3d703044bb7261be7e0d5ec250a16
Chase Bowden and John Rykken <john@>
2018-02-28 14:59:39 -0800

    Write, fail, pass callback spec/method.

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,4 +1,12 @@
 class Recipe < ActiveRecord::Base
   has_many :ingredients_recipes # points to join table
   has_many :ingredients, through: :ingredients_recipes # links ingredients
+  validates(:title, :presence => true)
+  before_save(:downcase_title)
+
+private
+  def downcase_title
+    self.title=(title().downcase)
+  end
+
 end
--- spec/recipe_spec.rb
+++ spec/recipe_spec.rb
@@ -3,5 +3,12 @@ require('pry')
 
 describe(Recipe) do
 
-
+  it "ensures input field is not empty" do
+    recipe = Recipe.create({:title => ""})
+    expect(recipe.save).to eq false
+  end
+  it("converts the name to lowercase") do
+    recipe = Recipe.create({:title => "CHICKEN PARM"})
+    expect(recipe.title()).to(eq("chicken parm"))
+  end
 end
