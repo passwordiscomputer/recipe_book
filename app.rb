b9ffc84c561eb8aaf1a824b4299b71c3b8c2845a
Chase Bowden and John Rykken <john@>
2018-02-28 16:07:57 -0800

    Add ingredient/tag pages. Finish writing app.rb functionality.

--- app.rb
+++ app.rb
@@ -5,6 +5,7 @@ also_reload("lib/**/*.rb")
 require("./lib/ingredients_recipes")
 require("./lib/ingredient")
 require("./lib/recipe")
+require("./lib/recipes_tags")
 require("./lib/tag")
 require("pry")
 
@@ -31,6 +32,7 @@ end
 get('/recipes/:id')do
   @recipe = Recipe.find(params.fetch("id").to_i())
   @ingredients = Ingredient.all - @recipe.ingredients
+  @tags = Tag.all - @recipe.tags
   erb(:recipe_edit)
 end
 
@@ -53,7 +55,13 @@ patch('/recipes/:id') do
     new_ingredient = Ingredient.find(ingredient_id)
     @recipe.ingredients.push(new_ingredient)
   end
+  tag_ids = params['tag_ids'] || []
+  tag_ids.each do |tag_id|
+    new_tag = Tag.find(tag_id)
+    @recipe.tags.push(new_tag)
+  end
   @ingredients = Ingredient.all - @recipe.ingredients
+  @tags = Tag.all - @recipe.tags
   erb(:recipe_edit)
 end
 
@@ -62,6 +70,7 @@ delete('/recipes/:id') do # deletes ingredient from recipe
   ingredient_id_delete = params[:ingredient_id_delete]
   @recipe.ingredients.destroy(ingredient_id_delete.to_i)
   @ingredients = Ingredient.all - @recipe.ingredients
+  @tags = Tag.all - @recipe.tags
   erb(:recipe_edit)
 end
 
@@ -69,6 +78,20 @@ get('/tags') do
   @tags = Tag.all
   erb(:tags)
 end
+
+get('/tags/:id') do
+  @tag = Tag.find(params[:id].to_i)
+  erb(:tag)
+end
+
+get('/ingredients/:id') do
+  @ingredient = Ingredient.find(params[:id].to_i)
+  erb(:ingredient)
+end
+
+
+
+
 
 
 
